6b960e80a8f25b635cf4a15e2b44cbf49866a3e8
FanFan <pamparau.stefan@gmail.com>
2016-05-08 15:17:23 +0300

    Added purchase CRUD logic.

--- webapp/src/main/java/com/iquestgroup/webApp/controllers/PurchaseController.java
+++ webapp/src/main/java/com/iquestgroup/webApp/controllers/PurchaseController.java
@@ -2,17 +2,16 @@ package com.iquestgroup.webApp.controllers;
 
 import com.iquestgroup.service.PurchaseService;
 import com.iquestgroup.service.exceptionHandling.ServiceException;
+import com.iquestgroup.webApp.annotations.HttpMethodType;
+import com.iquestgroup.webApp.annotations.Mapping;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -22,66 +21,58 @@ import javax.servlet.http.HttpServletResponse;
  * @author Stefan Pamparau
  */
 @Component("PurchaseController")
-@RequestMapping(path = "purchases")
 public class PurchaseController extends AbstractController {
     @Autowired
     private PurchaseService purchaseService;
 
-    @RequestMapping(path = "/list", method = RequestMethod.GET)
-    public ModelAndView listAllPurchases(HttpServletRequest request, HttpServletResponse response, Model model) {
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName("store/purchases/listPurchases");
-
+    @Mapping(path = "/purchases/list", method = HttpMethodType.GET)
+    public void listAllPurchases(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
-            mav.addObject("purchases", purchaseService.getAllPurchases());
+            request.setAttribute("purchases", purchaseService.getAllPurchases());
         } catch (ServiceException e) {
             e.printStackTrace();
         }
 
-        return mav;
+        request.getRequestDispatcher("/WEB-INF/views/pages/store/purchases/listPurchases.jsp").include(request, response);
     }
 
-    @RequestMapping(path = "/list/{clientAccountId}", method = RequestMethod.GET)
-    public ModelAndView listAllPurchasesForClientAccountId(HttpServletRequest request, HttpServletResponse response, @PathVariable Integer clientAccountId, Model model) {
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName("store/purchases/listPurchases");
+    @Mapping(path = "/purchases/list/\\d+", method = HttpMethodType.GET)
+    public void listAllPurchasesForClientAccountId(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String[] parts = request.getRequestURI().substring(request.getContextPath().length()).split("/");
 
         try {
-            mav.addObject("purchases", purchaseService.getPurchasesByClientAccountId(clientAccountId));
+            request.setAttribute("purchases", purchaseService.getPurchasesByClientAccountId(Integer.parseInt(parts[parts.length - 1])));
         } catch (ServiceException e) {
             e.printStackTrace();
         }
 
-        return mav;
+        request.getRequestDispatcher("/WEB-INF/views/pages/store/purchases/listPurchases.jsp").include(request, response);
     }
 
-    @RequestMapping(path = "/purchase", method = RequestMethod.GET)
-    public String displayPurchaseBookForm(HttpServletRequest request, HttpServletResponse response) {
-        return "store/purchases/purchaseBook";
+    @Mapping(path = "/purchases/purchase", method = HttpMethodType.GET)
+    public void displayPurchaseBookForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/views/pages/store/purchases/purchaseBook.jsp").include(request, response);
     }
 
-    @RequestMapping(path = "/purchase/{clientAccountId}", method = RequestMethod.GET)
-    public String displayPurchaseBookFormWithRetrievedClientId(HttpServletRequest request, HttpServletResponse response, @PathVariable Integer clientAccountId, Model model) {
-        model.addAttribute("clientAccountId", clientAccountId);
+    @Mapping(path = "/purchases/purchase/\\d+", method = HttpMethodType.GET)
+    public void displayPurchaseBookFormWithRetrievedClientId(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String[] parts = request.getRequestURI().substring(request.getContextPath().length()).split("/");
+        request.setAttribute("clientAccountId", parts[parts.length - 1]);
 
-        return "store/purchases/purchaseBook";
+        request.getRequestDispatcher("/WEB-INF/views/pages/store/purchases/purchaseBook.jsp").include(request, response);
     }
 
-    @RequestMapping(path = "/purchase", method = RequestMethod.POST)
-    public ModelAndView purchaseBook(HttpServletRequest request, HttpServletResponse response, @RequestParam("clientAccountId") Integer clientAccountId,
-                                     @RequestParam("bookId") Integer bookId) {
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName("store/purchases/listPurchases");
-
+    @Mapping(path = "/purchases/purchase", method = HttpMethodType.POST)
+    public void purchaseBook(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
-            String result = purchaseService.purchaseBook(clientAccountId, bookId);
+            String result = purchaseService.purchaseBook(Integer.parseInt(request.getParameter("clientAccountId")), Integer.parseInt(request.getParameter("bookId")));
 
-            mav.addObject("message", result);
-            mav.addObject("purchases", purchaseService.getAllPurchases());
+            request.setAttribute("message", result);
+            request.setAttribute("purchases", purchaseService.getAllPurchases());
         } catch (ServiceException e) {
             e.printStackTrace();
         }
 
-        return mav;
+        request.getRequestDispatcher("/WEB-INF/views/pages/store/purchases/listPurchases.jsp").include(request, response);
     }
 }
